d5369ae9526814dc87f609ecee74fd669436b3cc
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-08 16:09:02 +0100

    add Cypress test for user seeing product list add implementation

---
+++ cypress/integration/UserCanSeeACollectioOfProducts.feature.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-sequences */
+describe("User that visits the application", () => {
+  before(() => {
+    cy.intercept("GET", "**/api/products", { fixtures: "products.json" }).as(
+      "getProducts"
+    );
+    cy.visit("/");
+  });
+
+  it("is expected to make a network call woth status 200", () => {
+    cy.wait("@getProducts").its("response.statusCode").should("eq", 200);
+  });
+
+  it("is expected to see a collection of 3 products", () => {
+    cy.get("[data-cy=product-list]").children().should("have.length", 3);
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from "react";
+import Products from "./modules/Products";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    Products.index().then((data) => {
+      setProducts(data);
+    });
+  }, []);
+
+  const addToOrder = (id) => {
+    // We can now make a POST request to /api/orders
+  };
+
+  const productsList = products.map((product) => {
+    return (
+      <div key={product_id}>
+        {product.name} - {`${product.price} kr`}{" "}
+        <button onClick={() => addToOrder(product.id)}>Add to Order</button>
+      </div>
+    );
+  });
+
   return (
     <>
       <h1>Slowfood</h1>
+      <div data-cy="product-list">{productsList}</div>
     </>
   );
-}
+};
 
 export default App;
